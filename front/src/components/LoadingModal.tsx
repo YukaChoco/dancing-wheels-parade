780ab654d3bb5a2ddcb3031a0ef8c2f7f85e47c6
YukaChoco <sugar.ore.yk@gmail.com>
2024-02-18 01:40:13 +0900

    LoadingModalに忍者反映・修正

--- front/src/components/LoadingModal.tsx
+++ front/src/components/LoadingModal.tsx
@@ -1,12 +1,18 @@
+import color from "@/assets/color.svg";
+import './LoadingModal.css';
+
 export function LoadingModal({ isOpen }: { isOpen: boolean }): JSX.Element {
 
   return (
     <div
       aria-modal={isOpen}
       aria-label="loading modal"
-      className={`h-screen w-screen fixed top-0 left-0 flex items-center justify-center bg-light/60 z-10 ${isOpen ? "" : "hidden"}`}
+      className={`h-screen w-screen fixed top-0 left-0 flex flex-col items-center justify-center bg-light/80 z-20 ${isOpen ? "" : "hidden"}`}
     >
-      <div className="w-fit h-fit">Loading...</div>
+      <div className="loader">
+        <img width="100%" src={color} alt="ninzya_body" />
+      </div>
+      <span className="pt-12 h-fit font-bold text-3xl">Loading...</span>
     </div>
   );
 }
