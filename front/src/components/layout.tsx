153ec0a037a858c502dd6eaa32adeabdc41c0472
YukaChoco <sugar.ore.yk@gmail.com>
2024-02-18 05:17:44 +0900

    背景画像のタグの位置を修正

--- front/src/components/layout.tsx
+++ front/src/components/layout.tsx
@@ -7,10 +7,10 @@ export function Layout(): JSX.Element {
     <div>
       <Header />
       <main className="bg-light h-screen pt-20 lg:px-24 md:px-12 px-4" >
+        <img src={mono} alt="背景" className="w-fit opacity-5 fixed bottom-0 right-0 p-6 w-3/4 sm:w-fit sm:h-1/2" />
         <div>
           <Outlet />
         </div>
-        <img src={mono} alt="背景" className="w-fit opacity-5 fixed bottom-0 right-0 p-6 w-3/4 sm:w-fit sm:h-1/2" />
       </main>
     </div>
   );
