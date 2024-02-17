b8e3c8f8c6c6eef8000a7bd74b858ee92ef58b2d
Kitayama Leo <116560622+KitayamaLeo@users.noreply.github.com>
2024-02-17 17:00:11 +0000

    ヘッダー(文字/影/フォント)余裕があれば中央に寄せたい

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,8 +1,12 @@
-
 export function Header(): JSX.Element {
   return (
-    <header className="fixed top-0 w-screen bg-dark py-2 text-4xl px-6 text-light font-bold">
-      Header
+    <header className="fixed top w-screen bg-dark py-2 px-6 text-light font-bold flex justify-between items-center">
+      <div className="text-5xl align-self-start font-consolas text-shadow">
+        <span>TAXMAN</span>
+      </div>
+      <div className="text-center" style={{ fontSize: '1.2rem' }}> {/* 直接スタイルを適用してフォントサイズを大きくしている */}
+        <span>学生のための税金FAQサイト</span>
+      </div>
     </header>
   );
 }
