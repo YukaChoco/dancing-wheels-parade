e99c0605e462d8e31df91288a8d2459dfdd87a82
Kitayama Leo <116560622+KitayamaLeo@users.noreply.github.com>
2024-02-18 00:44:03 +0000

    コードレビュー後

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,12 +1,15 @@
 export function Header(): JSX.Element {
   return (
-    <header className="fixed top w-screen bg-dark py-2 px-6 text-light font-bold flex justify-between items-center">
-      <div className="text-5xl align-self-start font-consolas text-shadow">
+    <header className="fixed top-0 w-screen bg-dark py-2 px-6 text-light font-bold flex justify-between items-center">
+      <div className="text-3xl font-consolas text-shadow"> {/* 3xlサイズを使用 */}
         <span>TAXMAN</span>
       </div>
-      <div className="text-center" style={{ fontSize: '1.2rem' }}> {/* 直接スタイルを適用してフォントサイズを大きくしている */}
+      <div className="text-center text-lg">
         <span>学生のための税金FAQサイト</span>
       </div>
     </header>
   );
 }
+
+
+
