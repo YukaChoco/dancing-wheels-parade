783845eb195b55af99401f35ffb1f3d8734dd794
YukaChoco <sugar.ore.yk@gmail.com>
2024-02-18 11:01:15 +0900

    コンテンツの最小HeightをScreenに合わせる

--- front/src/components/layout.tsx
+++ front/src/components/layout.tsx
@@ -6,7 +6,7 @@ export function Layout(): JSX.Element {
   return (
     <div>
       <Header />
-      <main className="bg-light h-screen pt-20 lg:px-24 md:px-12 px-4" >
+      <main className="bg-light h-fit min-h-screen pt-20 lg:px-24 md:px-12 px-4 pb-12" >
         <img src={mono} alt="背景" className="w-fit opacity-5 fixed bottom-0 right-0 p-6 w-3/4 sm:w-fit sm:h-1/2" />
         <div>
           <Outlet />
