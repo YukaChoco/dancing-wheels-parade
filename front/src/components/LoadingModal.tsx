ed228091f5f14e4d528a68f6a871e08b696e6699
Kitayama Leo <116560622+KitayamaLeo@users.noreply.github.com>
2024-02-18 05:33:51 +0000

    忍者の手とヘッダー

--- front/src/components/LoadingModal.tsx
+++ front/src/components/LoadingModal.tsx
@@ -7,7 +7,7 @@ export function LoadingModal({ isOpen }: { isOpen: boolean }): JSX.Element {
     <div
       aria-modal={isOpen}
       aria-label="loading modal"
-      className={`h-screen w-screen fixed top-0 left-0 flex flex-col items-center justify-center bg-light/80 z-20 ${isOpen ? "" : "hidden"}`}
+      className={`h-screen w-screen fixed top-0 left-0 flex flex-col items-center justify-center bg-light/80 z-50 ${isOpen ? "" : "hidden"}`}
     >
       <div className="loader">
         <img width="100%" src={color} alt="ninzya_body" />
